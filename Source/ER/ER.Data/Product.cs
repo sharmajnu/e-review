382635ed14f08734f109be8a99dd32b3b5bb2a3c
sharmajnu <sharma.jnu@gmail.com>
2015-09-20 10:34:04 +0530

    Removed Old DataAccess Project and Added Test Project for data layer

--- Source/ER/ER.Data/Product.cs
+++ Source/ER/ER.Data/Product.cs
@@ -12,9 +12,7 @@ namespace ER.Data
         public string Name { get; set; }
         public int ProductCategoryId { get; set; }
         public ProductCategory Category { get; set; }
-        
         public List<ProductProperty> Properties { get; set; }
-        
         public List<Image> Images { get; set; }
         public List<YoutubeLink> YoutubeLinks { get; set; }
         public List<ProductSellerAssociation> SoldBy { get; set; }
---
+++ Source/ER/ER.DataLayer.Test/ProductRepositoryTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ER.DataLayer.Test
+{
+    [TestClass]
+    public class ProductRepositoryTests
+    {
+        [TestMethod]
+        public void SaveNewProduct()
+        {
+        }
+    }
+}
